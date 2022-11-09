55154d8dbb15011f2d3a1a46a132584c92a1a13a
Kevin Berlemont <kevin.berlemont@gmail.com>
2022-11-09 18:09:58 -0500

    uodate data folder

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ import plotly.express as px
 
 st.title('Homepage of the Sentiment Analysis of Bachelor In Paradise 2022')
 
+# add a description of the app
+st.write('This app is a dashboard to analyze the sentiment of Bachelor In Paradise 2022')
+st.write('The data was collected using the Twitter API and the sentiment was analyzed using a pre-trained neural network model from Hugging Face')
+
+st.subheader('Bachelor in Paradise')
+st.write('Bachelor in Paradise consists in a TV show were contestants date each other. Through the course of the show, the contestants are eliminated until only one couple remains. The couple then has the opportunity to get engaged.')
+
+st.write('This app will analyze the sentiment of the tweets of the contestants of Bachelor in Paradise 2022 in order to know if some events happened in the show influence the public opinion')
+
 dates = [ '09_27','10_04','10_11','10_17','10_18', '10_24', '10_25', '10_31']
 
 candidates = ['brandon', 'michael','romeo','shanae','jill','johnny','brittany','justin','hunter','sierra','hailey','kira','lace']
@@ -17,7 +26,7 @@ df_numbers = pd.DataFrame(columns=candidates, index=dates)
 df_sentiments = df_sentiments.append({'Candidate':'dummy','Neg Tweets':0,'Number Tweets':0,'Date':'dummy'}, ignore_index=True)
 
 for date in dates:
-    temp_df = pd.read_csv(f'./data/{date}.csv')
+    temp_df = pd.read_csv(f'./data_web_app/{date}.csv')
     for candidate in candidates:
         nbr_positive = 0
         nbr_negative = 0
@@ -33,8 +42,11 @@ for date in dates:
                     nbr_neutral += 1
                 count += 1
 
+        # only add if there are enough tweets about the candidate
+        if count > 100:
+            df_sentiments = df_sentiments.append({'Candidate':candidate,'Neg Tweets':nbr_negative,'Number Tweets':count,'Date':date}, ignore_index=True)
         # add a row to df_sentiments
-        df_sentiments = df_sentiments.append({'Candidate':candidate,'Neg Tweets':nbr_negative/(count),'Number Tweets':count,'Date':date}, ignore_index=True)
+        # df_sentiments = df_sentiments.append({'Candidate':candidate,'Neg Tweets':nbr_negative/(count),'Number Tweets':count,'Date':date}, ignore_index=True)
 
         # st.dataframe(df_sentiments)
         # df_sentiments.append(pd.DataFrame({'Candidate':candidate,'Neg Tweets':nbr_negative /( nbr_negative + nbr_positive + nbr_neutral),'Number Tweets':count,'Date':date}))
@@ -61,7 +73,7 @@ st.markdown('Let us first look at the evolution of the sentiment of the tweets a
 fig_men = px.line(df_men, x='Date', y='Neg Tweets', markers = True, color = 'Candidate', title = 'Proportion of negative tweets about men contestants', labels = {'index':'Date', 'value':'Proportion of negative tweets', 'variable':'Candidate'})
 st.plotly_chart(fig_men)
 
-st.markdown('Let us first look at the evolution of the sentiment of the tweets about women contestants thruogh the show:')
+st.markdown('Let us first look at the evolution of the sentiment of the tweets about women contestants through the show:')
 fig_women = px.line(df_women, x='Date', y='Neg Tweets', markers = True, color = 'Candidate', title = 'Proportion of negative tweets about women contestants', labels = {'index':'Date', 'value':'Proportion of negative tweets', 'variable':'Candidate'})
 st.plotly_chart(fig_women)
 
